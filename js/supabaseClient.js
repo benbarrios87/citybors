a1c26bb60ca0373d778c33f709cf0c2cc2fbdeac
benbarrios87 <benbarrios87@gmail.com>
2026-05-21 19:10:49 +0200

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "DIN_SUPABASE_URL";
-const SUPABASE_ANON_KEY = "DIN_ANON_KEY";
+const SUPABASE_URL = "https://mvgvmzkykevsdydwcigg.supabase.co/rest/v1/";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im12Z3Ztemt5a2V2c2R5ZHdjaWdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkzNzQxOTAsImV4cCI6MjA5NDk1MDE5MH0.y9c8nyUeFJvwlqsIdKoPwr41ynNpd4vAPUK82krxjp8";
 
 const supabaseClient = supabase.createClient(
   SUPABASE_URL,
